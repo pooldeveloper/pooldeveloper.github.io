b72b0d48737931e8627a1c4e9eefa60eb977728b
unknown <pooldeveloper08@gmail.com>
2021-06-18 21:53:09 -0500

    first commit

---
+++ js/glider.js
@@ -0,0 +1,8 @@
+new Glider(document.querySelector('.carousel-list'), {
+    slidesToShow: 1,
+    dots: '.carousel-indicators',
+    arrows: {
+      prev: '.carousel-left',
+      next: '.carousel-right'
+    }
+  });
